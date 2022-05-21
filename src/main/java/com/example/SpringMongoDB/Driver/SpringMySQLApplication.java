69cb1c3396ca2914d25723726229dfb5a10cd18c
BloodySaint07 <66734010+BloodySaint07@users.noreply.github.com>
2022-05-21 13:38:36 +0530

    Exclude Hibernate Config Class

--- src/main/java/com/example/SpringMongoDB/Driver/SpringMySQLApplication.java
+++ src/main/java/com/example/SpringMongoDB/Driver/SpringMySQLApplication.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
@@ -21,7 +22,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
 @EnableCaching
 @EnableBatchProcessing
 @EnableJms
