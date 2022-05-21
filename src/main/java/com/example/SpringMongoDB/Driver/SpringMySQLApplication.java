096165408a9b6b9379b0ffbd1a83cc8135307302
BloodySaint07 <66734010+BloodySaint07@users.noreply.github.com>
2022-05-21 13:52:00 +0530

    Including Hibernate Again

--- src/main/java/com/example/SpringMongoDB/Driver/SpringMySQLApplication.java
+++ src/main/java/com/example/SpringMongoDB/Driver/SpringMySQLApplication.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
@@ -22,7 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @EnableCaching
 @EnableBatchProcessing
 @EnableJms
